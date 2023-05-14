737e53253bd2c205171a89124e1fe3bddea7a796
ngntuli <mr.ng.ntuli@gmail.com>
2023-05-14 14:48:46 +0200

    character promotion

--- src/main/java/com/ngntuli/basics/BasicsDemo.java
+++ src/main/java/com/ngntuli/basics/BasicsDemo.java
@@ -151,18 +151,28 @@ public class BasicsDemo {
 		System.out.println("x %= 5: " + (x %= 5));
 
 		// Invalid
-		System.out.println("x =+ 5: " + (x =+ 5)); // Unary plus ~ x = +5
-		System.out.println("x =- 5: " + (x =- 5)); // Unary minus ~ x = -5
-		
+		System.out.println("x =+ 5: " + (x = +5)); // Unary plus ~ x = +5
+		System.out.println("x =- 5: " + (x = -5)); // Unary minus ~ x = -5
+
 		/*
-		 * System.out.println("x =* 5: " + (x =* 5));
-		 * System.out.println("x =/ 5: " + (x =/ 5));
-		 * System.out.println("x =% 5: " + (x =% 5));
+		 * System.out.println("x =* 5: " + (x =* 5)); System.out.println("x =/ 5: " + (x
+		 * =/ 5)); System.out.println("x =% 5: " + (x =% 5));
 		 */
 	}
-	
+
 	static void isOddOrEven(int num) {
 		System.out.println(num % 2);
+	}
+
+	static void charTypePromotion() {
+		System.out.println("\nInside charTypePromotion ...");
+		char char1 = 50; // Will be assigned corresponding UTF16 value 2
+		System.out.println("char1: " + char1);
+		System.out.println("(73 - char1): " + (73 - char1)); // char1 gets promoted to int, i.e., decimal equivalent 50
+																// in UTF16 is used
+		System.out.println("(char1 - '3'): " + (char1 - '3')); // char1 & '3' are promoted to ints
+		System.out.println("('a' + 'b'): " + ('a' + 'b')); // 'a' & 'b' are promoted to ints and the respective
+															// equivalents 97 & 98 are added
 	}
 
 	public static void main(String[] args) {
@@ -175,7 +185,8 @@ public class BasicsDemo {
 		// printVarargsOverload();
 		// preAndPost();
 		// compoundArithmeticAssignment();
-		isOddOrEven(42);
+		// isOddOrEven(42);
+		charTypePromotion();
 	}
 
 }
