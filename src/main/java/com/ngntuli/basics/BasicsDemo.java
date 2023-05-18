4de983af8caf6767c6164bd30b31de3e014bf018
ngntuli <mr.ng.ntuli@gmail.com>
2023-05-18 23:44:34 +0200

    string pool

--- src/main/java/com/ngntuli/basics/BasicsDemo.java
+++ src/main/java/com/ngntuli/basics/BasicsDemo.java
@@ -461,6 +461,22 @@ public class BasicsDemo {
 		System.out.println("\ns.repeat(6): " + s.repeat(6));
 	}
 
+	static void stringPool() {
+		System.out.println("\n*** Inside stringPool ...***");
+		String s1 = "hello!";
+		String s2 = "hello!";
+		String s3 = "hello!".intern();
+		String s4 = new String("hello!");
+		String s5 = "lo!";
+
+		System.out.println("s1 == s2: " + (s1 == s2));
+		System.out.println("s1 == s3: " + (s1 == s3));
+		System.out.println("s1 == s4: " + (s1 == s4));
+		System.out.println("s1 == s4.intern(): " + (s1 == s4.intern()));
+		System.out.println("s1 == \"hel\" + \"lo!\": " + (s1 == "hel" + "lo!"));
+		System.out.println("s1 == \"hel\" + s5: " + (s1 == "hel" + s5));
+	}
+
 	public static void main(String[] args) {
 		// Language Basics 1
 		// print();
@@ -483,7 +499,8 @@ public class BasicsDemo {
 		// labeledContinue();
 		// import demo
 		// package creation demo
-		stringExamples();
+		// stringExamples();
+		stringPool();
 	}
 
 }
