2b75cc2f8c64a31a893b91dd40f3b964f13fc210
ngntuli <mr.ng.ntuli@gmail.com>
2023-05-19 00:00:08 +0200

    test for information hiding

--- src/main/java/com/ngntuli/basics/StudentTest.java
+++ src/main/java/com/ngntuli/basics/StudentTest.java
@@ -6,48 +6,22 @@ class StudentTest {
 		int[] studentIds = new int[] { 1001, 1002, 1003 };
 
 		// Creating first student object and setting its state
-		Student student1 = new Student(studentIds[0], "Uhando");
-		student1.gender = "female";
+		Student student1 = new Student(studentIds[0], "Uhando", "female");
 
 		// Creating second student object and setting its state
-		Student student2 = new Student(studentIds[1], "Charlotte");
-		student2.gender = "female";
+		Student student2 = new Student(studentIds[1], "Charlotte", "female");
 
 		// Creating third student object and setting its state
-		Student student3 = new Student(studentIds[2], "Nkosinathi");
-		student3.gender = "male";
+		Student student3 = new Student(studentIds[2], "Nkosinathi", "other");
 
 		// Print each students name
-		System.out.println("Name of student1: " + student1.name);
-		System.out.println("Name of student2: " + student2.name);
-		System.out.println("Name of student3: " + student3.name);
+		System.out.println("Name of student1: " + student1.getName());
+		System.out.println("Name of student2: " + student2.getName());
+		System.out.println("Name of student3: " + student3.getName());
 
 		// Update student1's name from Uhando to john by invoking updateProfile method
-		student1.updateProfile("Uthando");
-		System.out.println("Updated name of student1: " + student1.name);
-
-		Student student4 = student1;
-		System.out.println("Name of student4: " + student4.name);
-
-		student4.updateProfile("Doctor");
-		System.out.println("Name of student1: " + student1.name);
-
-		student4 = student2;
-		System.out.println("Name of student4: " + student4.name);
-
-		student2 = student1;
-		System.out.println("Name of student4: " + student4.name);
-		System.out.println("Name of student2: " + student2.name);
-
-		student4 = new Student("David");
-		System.out.println("Name of student4: " + student4.name);
-		System.out.println("Name of student2: " + student2.name);
-		System.out.println("Name of student1: " + student1.name);
-		System.out.println();
-		System.out.println("Name of student1: " + student1.name);
-		System.out.println("Name of student2: " + student2.name);
-		System.out.println("Name of student3: " + student3.name);
-		System.out.println("Name of student4: " + student4.name);
+		student1.setName("Uthando");
+		System.out.println("Updated name of student1: " + student1.getName());
 
 	}
 
