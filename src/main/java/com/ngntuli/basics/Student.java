a5fc0074bac9640c8b0f15edeaa6c3f422f9177a
ngntuli <mr.ng.ntuli@gmail.com>
2023-05-19 20:21:46 +0200

    static variable to count student created

--- src/main/java/com/ngntuli/basics/Student.java
+++ src/main/java/com/ngntuli/basics/Student.java
@@ -2,6 +2,8 @@ package com.ngntuli.basics;
 
 class Student {
 	// variable declarations
+	private static int studentCount;
+
 	private int id;
 	private String name;
 	private String gender = "male";
@@ -14,11 +16,16 @@ class Student {
 		this.name = name;
 	}
 
+	public static int getStudentCount() {
+		return studentCount;
+	}
+
 	// Constructors
 	public Student(int id, String name, String gender) {
 		this.id = id;
 		this.name = name;
 		this.gender = gender;
+		studentCount++;
 	}
 
 	// method definitions
