0d80ce9613f0aa33ca92abe93a0abbcb532c25bb
ngntuli <mr.ng.ntuli@gmail.com>
2023-06-03 19:54:10 +0200

    concrete class extends abstract class

---
+++ src/main/java/com/ngntuli/oop/absrt/ConcreteSubclass.java
@@ -0,0 +1,23 @@
+package com.ngntuli.oop.absrt;
+
+public class ConcreteSubclass extends AbstractSubclass {
+
+	@Override
+	void test2() {
+		System.out.println("test2");
+	}
+
+	@Override
+	void test3() {
+		System.out.println("test3");
+	}
+
+	public static void main(String[] args) {
+		ConcreteSubclass concrete = new ConcreteSubclass();
+		concrete.test1();
+		concrete.test2();
+		concrete.test3();
+		//AbstractSuperclass test = new AbstractSuperclass(); // Cannot instantiate the type abstract
+	}
+
+}
