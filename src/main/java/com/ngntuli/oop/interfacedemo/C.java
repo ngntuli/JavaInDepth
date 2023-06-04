d1c6ead3140ca86b1dc22faac6f2188840475c46
ngntuli <mr.ng.ntuli@gmail.com>
2023-06-04 21:32:06 +0200

    defaults methods

--- src/main/java/com/ngntuli/oop/interfacedemo/C.java
+++ src/main/java/com/ngntuli/oop/interfacedemo/C.java
@@ -1,5 +1,18 @@
 package com.ngntuli.oop.interfacedemo;
 
+/**
+ * 
+ * @author ABC Inc.
+ *
+ */
 public interface C extends A {
 	void foobar();
+
+	default void go() {
+		System.out.println("C: go");
+	}
+
+	default void run() {
+		System.out.println("C: run");
+	}
 }
