97800748533c19f8b304bd15f565fc99472f43b1
ngntuli <mr.ng.ntuli@gmail.com>
2023-06-04 21:32:25 +0200

    test default methods

--- src/main/java/com/ngntuli/oop/interfacedemo/TestClient.java
+++ src/main/java/com/ngntuli/oop/interfacedemo/TestClient.java
@@ -1,19 +1,30 @@
 package com.ngntuli.oop.interfacedemo;
 
+/**
+ * 
+ * @author Client Inc.
+ *
+ */
 public class TestClient {
 	public static int getVal() {
 		return 42;
 	}
 
 	public static void main(String[] args) {
+		// Interface demo
 		C c = new X();
-		//c.foo();
-		//c.bar();
-		//c.foobar();
-		
-		   C clone = ((X)c).clone();
-		   if (clone != c) {
-		      System.out.println("Clone created!!");
-		   }
+		// c.foo();
+		// c.bar();
+		// c.foobar();
+
+		// Clone demo
+		// C clone = ((X) c).clone();
+		// if (clone != c) {
+		// System.out.println("Clone created!!");
+		// }
+
+		// Default method demo
+		c.go();
+		c.run();
 	}
 }
