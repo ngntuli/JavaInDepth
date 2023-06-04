43ab2584d71ed256fdb2f72e26b5a8ccaab1eeab
ngntuli <mr.ng.ntuli@gmail.com>
2023-06-04 20:14:12 +0200

    test client class

---
+++ src/main/java/com/ngntuli/oop/interfacedemo/TestClient.java
@@ -0,0 +1,14 @@
+package com.ngntuli.oop.interfacedemo;
+
+public class TestClient {
+	public static int getVal() {
+		return 42;
+	}
+
+	public static void main(String[] args) {
+		C c = new X();
+		c.foo();
+		c.bar();
+		c.foobar();
+	}
+}
