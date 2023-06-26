e79869760c17a6fe1fc84ce80fdf7087982946aa
ngntuli <mr.ng.ntuli@gmail.com>
2023-06-26 19:04:49 +0200

    updates exceptions demo

--- src/main/java/com/ngntuli/oop/exceptions/ExceptionsDemo.java
+++ src/main/java/com/ngntuli/oop/exceptions/ExceptionsDemo.java
@@ -1,10 +1,11 @@
 package com.ngntuli.oop.exceptions;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class ExceptionsDemo {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 		System.out.println("\nInside main ...");
 		// try {
 		share();
@@ -16,13 +17,19 @@ public class ExceptionsDemo {
 		System.out.println("\nEnd of main ...");
 	}
 
-	private static void share() throws FileNotFoundException {
+	private static void share() throws IOException {
 		System.out.println("\nInside share ...");
 
 		try {
-			HttpConnect.send(0, "hello", "http://www.spring-boot.com");
+			HttpConnect.send(1, "hello", "http://www.spring-boot.com");
 		} catch (FileNotFoundException e) {
 			throw e;
+		} catch (IOException e) {
+			System.out.println("Connecting to a different server ...");
+		} catch (Exception e) {
+			// TODO: handle exception
+		} catch (Throwable e) {
+			// TODO: handle exception
 		}
 
 		System.out.println("\nEnd of share ...");
