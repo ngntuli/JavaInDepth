52f2a31139fc3fdac8f670da69e5ac775661347d
ngntuli <mr.ng.ntuli@gmail.com>
2023-06-26 18:54:18 +0200

    throw io exception if dest equals to one

--- src/main/java/com/ngntuli/oop/exceptions/HttpConnect.java
+++ src/main/java/com/ngntuli/oop/exceptions/HttpConnect.java
@@ -1,13 +1,16 @@
 package com.ngntuli.oop.exceptions;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class HttpConnect {
 
-	public static void send(int destination, String data, String partner) throws FileNotFoundException {
+	public static void send(int destination, String data, String partner) throws IOException {
 		System.out.println("\nInside send ...");
 		if (destination == 0) {
 			throw new FileNotFoundException();
+		} else if (destination == 1) {
+			throw new IOException();
 		}
 
 		System.out.println("\nEnd of send ...");
