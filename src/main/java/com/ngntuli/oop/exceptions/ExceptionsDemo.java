e7d98ec1fe72302d41fdd6cb4c95ad9ef0a66888
ngntuli <mr.ng.ntuli@gmail.com>
2023-07-06 23:53:32 +0200

    use finally block exception

--- src/main/java/com/ngntuli/oop/exceptions/ExceptionsDemo.java
+++ src/main/java/com/ngntuli/oop/exceptions/ExceptionsDemo.java
@@ -7,25 +7,31 @@ public class ExceptionsDemo {
 
 	public static void main(String[] args) throws IOException {
 		System.out.println("\nInside main ...");
-		// try {
-		share();
-
-		// } catch (FileNotFoundException e) {
-		// e.printStackTrace();
-		// }
-
+		try {
+			share();
+			System.out.println("After invoking share ...");
+		} catch (FileNotFoundException e) {
+			// e.printStackTrace();
+			System.out.println("main: FileNotFoundException ...");
+		} finally {
+			System.out.println("Inside main's finally ...");
+		}
 		System.out.println("\nEnd of main ...");
 	}
 
-	private static void share() throws IOException {
+	private static void share() throws FileNotFoundException {
 		System.out.println("\nInside share ...");
 
 		try {
-			HttpConnect.send(-1, "hello", "http://www.spring-boot.com");
+			HttpConnect.send(0, "hello", "http://www.spring-boot.com");
+			System.out.println("After invoking send ...");
 		} catch (FileNotFoundException e) {
+			System.out.println("Share: FileNotFoundException ...");
 			throw e;
 		} catch (IOException e) {
 			System.out.println("Connecting to a different server ...");
+		} finally {
+			System.out.println("Inside share's finally ...");
 		}
 
 		System.out.println("\nEnd of share ...");
