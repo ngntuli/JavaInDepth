12ef5c47ee269b865c3d783845374f754403f319
ngntuli <mr.ng.ntuli@gmail.com>
2023-07-07 15:17:46 +0200

    add two methods

--- src/main/java/com/ngntuli/oop/exceptions/TryWithResourcesDemo.java
+++ src/main/java/com/ngntuli/oop/exceptions/TryWithResourcesDemo.java
@@ -7,8 +7,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class TryWithResourcesDemo {
-	static String inFileStr = "walden.jpg";
-	static String outFileStr = "walden-out.jpg";
+	static String inFileStr = "pacman.jpeg";
+	static String outFileStr = "pacman-out.jpeg";
 
 	public static void fileCopyWithArm() throws IOException {
 		System.out.println("\nInside fileCopyWithArm ...");
@@ -27,9 +27,149 @@ public class TryWithResourcesDemo {
 		}
 	}
 
+	public static void fileCopyWithoutArm() throws IOException {
+		System.out.println("\nInside fileCopyWithoutArm ...");
+
+		Test t = null;
+		Test2 t2 = null;
+		BufferedInputStream in = null;
+		BufferedOutputStream out = null;
+
+		try {
+			t = new Test();
+			t2 = new Test2();
+			in = new BufferedInputStream(new FileInputStream(inFileStr));
+			out = new BufferedOutputStream(new FileOutputStream(outFileStr));
+
+			byte[] byteBuf = new byte[4000];
+			int numBytesRead;
+			while ((numBytesRead = in.read(byteBuf)) != -1) {
+				out.write(byteBuf, 0, numBytesRead);
+			}
+
+			throw new IOException("Important Exception!!");
+
+		} finally {
+
+			if (t2 != null)
+				t2.close();
+
+			if (t != null)
+				t.close();
+
+			if (in != null)
+				in.close();
+
+			if (out != null)
+				out.close();
+		}
+
+	}
+
+	// Handles Exception Masking via Suppression
+	public static void fileCopyWithoutArm2() throws IOException {
+		System.out.println("\nInside fileCopyWithoutArm2 ...");
+
+		Test t = null;
+		Test2 t2 = null;
+		BufferedInputStream in = null;
+		BufferedOutputStream out = null;
+
+		IOException ioException = null;
+
+		try {
+			t = new Test();
+			t2 = new Test2();
+			in = new BufferedInputStream(new FileInputStream(inFileStr));
+			out = new BufferedOutputStream(new FileOutputStream(outFileStr));
+
+			byte[] byteBuf = new byte[4000];
+			int numBytesRead;
+			while ((numBytesRead = in.read(byteBuf)) != -1) {
+				out.write(byteBuf, 0, numBytesRead);
+			}
+
+			throw new IOException("Important Exception!!");
+
+		} catch (IOException e) {
+			ioException = e;
+		} finally {
+			if (ioException != null) {
+				try {
+					if (t2 != null) {
+						t2.close();
+					}
+				} catch (IOException e) {
+					ioException.addSuppressed(e);
+				}
+			} else {
+				try {
+					if (t2 != null) {
+						t2.close();
+					}
+				} catch (IOException e) {
+					ioException = e;
+				}
+			}
+
+			if (ioException != null) {
+				try {
+					if (t != null) {
+						t.close();
+					}
+				} catch (IOException e) {
+					ioException.addSuppressed(e);
+				}
+			} else {
+				try {
+					if (t != null) {
+						t.close();
+					}
+				} catch (IOException e) {
+					ioException = e;
+				}
+			}
+
+			if (ioException != null) {
+				try {
+					if (in != null) {
+						in.close();
+					}
+				} catch (IOException e) {
+					ioException.addSuppressed(e);
+				}
+			} else {
+				try {
+					if (in != null) {
+						in.close();
+					}
+				} catch (IOException e) {
+					ioException = e;
+				}
+			}
+
+			if (ioException != null) {
+				try {
+					if (out != null) {
+						out.close();
+					}
+				} catch (IOException e) {
+					ioException.addSuppressed(e);
+				}
+
+				throw ioException;
+			} else {
+				if (out != null) {
+					out.close();
+				}
+			}
+		}
+
+	}
+
 	public static void main(String[] args) {
 		try {
-			fileCopyWithArm();
+			fileCopyWithoutArm2();
 		} catch (IOException e) {
 			e.printStackTrace();
 
