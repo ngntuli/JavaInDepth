d8f4cb131abf769b218d41fcb2a252991a5c4589
ngntuli <mr.ng.ntuli@gmail.com>
2023-07-07 21:48:52 +0200

    updates comments code

--- src/main/java/com/ngntuli/oop/exceptions/APIFormatChangeException.java
+++ src/main/java/com/ngntuli/oop/exceptions/APIFormatChangeException.java
@@ -1,5 +1,30 @@
 package com.ngntuli.oop.exceptions;
 
 public class APIFormatChangeException extends Exception {
+	private String response;
+	private String elementName;
+	private String partner;
 
+	public APIFormatChangeException(String response, String elementName, String partner) {
+		super("Response: " + response + ", Element: " + elementName + ", Partner: " + partner);
+		this.response = response;
+		this.elementName = elementName;
+		this.partner = partner;
+	}
+
+	/*
+	 * public APIFormatChangeException(String message) { super(message); }
+	 */
+
+	public String getResponse() {
+		return response;
+	}
+
+	public String getElementName() {
+		return elementName;
+	}
+
+	public String getPartner() {
+		return partner;
+	}
 }
