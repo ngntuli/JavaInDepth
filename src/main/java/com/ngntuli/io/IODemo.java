80cc38bf8b158fa91669006aafad9cb8cb5b0f11
ngntuli <mr.ng.ntuli@gmail.com>
2023-07-22 21:49:04 +0200

    read from standard input

--- src/main/java/com/ngntuli/io/IODemo.java
+++ src/main/java/com/ngntuli/io/IODemo.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 import java.util.HexFormat;
+import java.util.Scanner;
 
 public class IODemo {
 	static String inFileStr = "pacman.jpeg";
@@ -102,12 +103,51 @@ public class IODemo {
 		}
 	}
 
+	private static void readFromStandardInput() {
+		System.out.println("\nInside readFromStandardInput ...");
+		String data;
+		/*
+		 * System.out.print("Enter \"start\" to continue (Using BufferedReader): ");
+		 * 
+		 * try (BufferedReader in = new BufferedReader(new InputStreamReader(System.in,
+		 * "UTF-8"))){ while ((data = in.readLine()) != null && !data.equals("start")) {
+		 * System.out.print("\nDid not enter \"start\". Try again: "); } } catch
+		 * (IOException e) { e.printStackTrace(); } System.out.println("Correct!!");
+		 */
+
+		System.out.print("\nEnter \"start\" to continue (Using java.util.Scanner): ");
+		Scanner scanner = new Scanner(System.in);
+
+		while (!(data = scanner.nextLine()).equals("start")) {
+			System.out.print("\nDid not enter \"start\". Try again: ");
+		}
+		System.out.println("Correct!!");
+
+		System.out.println("Now, enter the start code: ");
+		int code = scanner.nextInt(); // other methods: nextLong, nextDouble, etc
+		System.out.println("Thanks. You entered code: " + code);
+
+		/**
+		 * Scanner ~ a text scanner for parsing primitives & string ~ breaks its input
+		 * into tokens using a delimited pattern (default: whitespace) ~ when System.in
+		 * is used, internally constructor uses an InputStreamReader to read from it ~
+		 * hasXXX & nextXXX can be used together ~ InputMismatchException is thrown ~
+		 * From Java 5 onwards
+		 */
+
+		Scanner s1 = new Scanner("Hello, How are you?");
+		while (s1.hasNext()) {
+			System.out.println(s1.next());
+		}
+	}
+
 	public static void main(String[] args) {
 		// applyEncoding();
 		// fileMethods();
 		// fileCopyNoBuffer();
 		// fileCopyWithBufferAndArray();
-		getProperties();
+		// getProperties();
+		readFromStandardInput();
 
 	}
 }
