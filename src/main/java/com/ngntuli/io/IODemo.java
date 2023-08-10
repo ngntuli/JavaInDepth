764cc9956b1ca39b2b9bc863140645ada2da25e3
ngntuli <mr.ng.ntuli@gmail.com>
2023-08-10 16:14:17 +0200

    encoding sync method

--- src/main/java/com/ngntuli/io/IODemo.java
+++ src/main/java/com/ngntuli/io/IODemo.java
@@ -2,11 +2,13 @@ package com.ngntuli.io;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.UnsupportedEncodingException;
@@ -228,6 +230,25 @@ public class IODemo {
 		}
 	}
 
+	public static void encodingSync() {
+		/*try (BufferedReader br = new BufferedReader(
+				new InputStreamReader(new FileInputStream("encoding"), "UTF-16BE"))) {
+			System.out.println(br.readLine());
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}*/
+
+		try {
+			 //System.out.println(new String("€".getBytes("UTF-8"), "UTF-16BE"));
+			System.out.println(new String("a".getBytes("US-ASCII"), "UTF-16BE"));
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	public static void main(String[] args) {
 		// applyEncoding();
 		// fileCopyNoBuffer();
@@ -238,10 +259,12 @@ public class IODemo {
 		// dirFilter(true);
 
 		// Serialization
-		if (args.length > 0 && args[0].equals("true")) {
-			new IODemo().doSerialization();
-		}
-		new IODemo().doDeserialization();
+		// if (args.length > 0 && args[0].equals("true")) {
+		// new IODemo().doSerialization();
+		// }
+		// new IODemo().doDeserialization();
+
+		encodingSync();
 
 	}
 }
