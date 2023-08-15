9c2a4a11e148ccb422a4d1c860b150b4a5c13189
ngntuli <mr.ng.ntuli@gmail.com>
2023-08-15 22:54:07 +0200

    add sum static field

--- src/main/java/com/ngntuli/collections/Filter.java
+++ src/main/java/com/ngntuli/collections/Filter.java
@@ -3,6 +3,7 @@ package com.ngntuli.collections;
 import java.util.function.Consumer;
 
 public class Filter implements Consumer<Object> {
+	private static int sum = 0;
 
 	@Override
 	public void accept(Object o) {
@@ -12,7 +13,9 @@ public class Filter implements Consumer<Object> {
 	}
 
 	public static void add(Integer i) {
-		System.out.println(i + 7);
+		int num1 = sum;
+		sum = sum + i;
+		System.out.println(num1 + " + " + i + " = " + sum);
 	}
 
 	public static void filter(Integer i) {
