cd426bc30e8e9681f21c68c8aca2119bff42aa0b
ngntuli <mr.ng.ntuli@gmail.com>
2023-08-18 23:32:16 +0200

    NavigableSet, SortedSet and TreeSet

--- src/main/java/com/ngntuli/collections/SetDemo.java
+++ src/main/java/com/ngntuli/collections/SetDemo.java
@@ -2,7 +2,9 @@ package com.ngntuli.collections;
 
 import java.util.HashSet;
 import java.util.LinkedHashSet;
+import java.util.NavigableSet;
 import java.util.Set;
+import java.util.SortedSet;
 import java.util.TreeSet;
 
 public class SetDemo {
@@ -54,9 +56,49 @@ public class SetDemo {
 		}
 	}
 
+	private static void treeSetDemo2() {
+		NavigableSet<Integer> set = new TreeSet<>();
+		set.add(3);
+		set.add(33);
+		set.add(74);
+		set.add(89);
+
+		System.out.println("lower: " + set.lower(74));
+		System.out.println("floor: " + set.floor(74));
+		System.out.println("ceiling: " + set.ceiling(74));
+		System.out.println("higher: " + set.higher(74));
+
+		System.out.println("first: " + set.first());
+		System.out.println("last: " + set.last());
+
+		System.out.println("set: " + set);
+
+		NavigableSet<Integer> descendingSet = set.descendingSet();
+		System.out.println("descendingSet: " + descendingSet);
+
+		NavigableSet<Integer> headSet = set.headSet(74, true);
+		System.out.println("headSet: " + headSet);
+
+		headSet.add(6);
+		System.out.println("headSet: " + headSet);
+		System.out.println("set: " + set);
+		headSet.add(4);
+		// headSet.add(75); // throws IllegalArgumentException
+		System.out.println("set: " + set);
+
+		SortedSet<Integer> subSet = set.subSet(5, 74);
+		// subSet.add(2); // throws IllegalArgumentException
+
+		// Adding element in backed set (original set) and
+		// see it getting reflected in subSet
+		set.add(25);
+		System.out.println("subSet: " + subSet);
+	}
+
 	public static void main(String[] args) {
 		// hashSetDemo();
 		// linkedHashSetDemo();
-		treeSetDemo();
+		// treeSetDemo();
+		treeSetDemo2();
 	}
 }
