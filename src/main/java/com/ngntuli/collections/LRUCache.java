50175284008cbdb3b3ce1303413d120574cf3e9a
ngntuli <mr.ng.ntuli@gmail.com>
2023-08-22 18:48:54 +0200

    update LRU Cache

--- src/main/java/com/ngntuli/collections/LRUCache.java
+++ src/main/java/com/ngntuli/collections/LRUCache.java
@@ -6,14 +6,15 @@ import java.util.Map;
 import java.util.Set;
 
 public class LRUCache<K, V> extends LinkedHashMap<K, V> {
-	// private static final long serialVersionUID = 1L;
-	private static final int MAX_ENTRIES = 3;
+	private static final long serialVersionUID = 1L;
+	private static final int MAX_ENTRIES = 2;
 
 	public LRUCache(int initialCapacity, float loadFactor, boolean accessOrder) {
 		super(initialCapacity, loadFactor, accessOrder);
 	}
 
 	// Invoked by put and putAll after inserting a new entry into the map
+	@Override
 	public boolean removeEldestEntry(Map.Entry eldest) {
 		return size() > MAX_ENTRIES;
 		// return false; // same as normal linked hash map
