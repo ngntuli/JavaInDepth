47ab0dd6d69d615e478e0e70114b78fdeb746e01
ngntuli <mr.ng.ntuli@gmail.com>
2023-08-31 22:08:32 +0200

    half integrator class

---
+++ src/main/java/com/ngntuli/generics/HalfIntegrator.java
@@ -0,0 +1,10 @@
+package com.ngntuli.generics;
+
+import java.util.List;
+
+public class HalfIntegrator {
+	public static void getPrice(int ISBN, List prices) {
+		System.out.println("ISBN:" + ISBN);
+		prices.add(45);
+	}
+}
