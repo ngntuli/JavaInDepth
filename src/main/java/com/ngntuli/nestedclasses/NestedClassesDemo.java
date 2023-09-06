466b448f012fc18b9253ace077abf0979ff097cb
ngntuli <mr.ng.ntuli@gmail.com>
2023-09-06 21:03:31 +0200

    sort bookmarks by rating

--- src/main/java/com/ngntuli/nestedclasses/NestedClassesDemo.java
+++ src/main/java/com/ngntuli/nestedclasses/NestedClassesDemo.java
@@ -1,6 +1,7 @@
 package com.ngntuli.nestedclasses;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class NestedClassesDemo {
 	public static void main(String[] args) {
@@ -9,22 +10,27 @@ public class NestedClassesDemo {
 		Bookmark item1 = new Bookmark();
 		item1.setId(2000);
 		item1.setTitle("Use Final Liberally");
+		item1.setRating(1.0);
 
 		Bookmark item2 = new Bookmark();
 		item2.setId(2001);
 		item2.setTitle("How do I import a pre-existing Java project into Eclipse and get up and running?");
+		item2.setRating(2.0);
 
 		Bookmark item3 = new Bookmark();
 		item3.setId(2002);
 		item3.setTitle("Interface vs Abstract Class");
+		item3.setRating(3.0);
 
 		Bookmark item4 = new Bookmark();
 		item4.setId(2003);
 		item4.setTitle("NIO tutorial by Greg Travis");
+		item4.setRating(4.0);
 
 		Bookmark item5 = new Bookmark();
 		item5.setId(2004);
 		item5.setTitle("Virtual Hosting and Tomcat");
+		item5.setRating(5.0);
 
 		recommendedItems.add(item1);
 		recommendedItems.add(item2);
@@ -39,7 +45,12 @@ public class NestedClassesDemo {
 			System.out.println(iterator.next().getTitle());
 		}
 
-		Arrays.sort(recommendedItems.getItems());
+		Arrays.sort(recommendedItems.getItems(), new Comparator<Bookmark>() {
+			@Override
+			public int compare(Bookmark o1, Bookmark o2) {
+				return o1.getRating() < o2.getRating() ? 1 : -1;
+			}
+		});
 		System.out.println("\nSorted by rating ...");
 		iterator = recommendedItems.iterator();
 
