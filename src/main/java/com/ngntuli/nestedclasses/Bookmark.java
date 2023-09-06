ab2634f4b3adefe1fda6e2dec4dcdb7d89d2602b
ngntuli <mr.ng.ntuli@gmail.com>
2023-09-06 21:49:09 +0200

    Sorted by rating (Static Member Class)

--- src/main/java/com/ngntuli/nestedclasses/Bookmark.java
+++ src/main/java/com/ngntuli/nestedclasses/Bookmark.java
@@ -1,9 +1,26 @@
 package com.ngntuli.nestedclasses;
 
+import java.io.Serializable;
+import java.util.Comparator;
+
 public class Bookmark {
 	private long id;
 	private String title;
 	private double rating;
+
+	// strategy
+	public static final Comparator<Bookmark> RATING_COMPARATOR = new RatingComparator();
+
+	private static class RatingComparator implements Comparator<Bookmark>, Serializable {
+
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public int compare(Bookmark o1, Bookmark o2) {
+			return o1.getRating() < o2.getRating() ? 1 : -1;
+		}
+
+	}
 
 	public long getId() {
 		return id;
