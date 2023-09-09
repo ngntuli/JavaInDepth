fc4d510200a8dfd9049862c1b65fc8ba66a444d9
ngntuli <mr.ng.ntuli@gmail.com>
2023-09-09 15:10:55 +0200

    enum BookGenre

---
+++ src/main/java/com/ngntuli/enums/BookGenre.java
@@ -0,0 +1,16 @@
+package com.ngntuli.enums;
+
+public enum BookGenre {
+	BIOGRAPHY, HORROR;
+
+	public static void main(String[] args) {
+		for (BookGenre bookGenre : BookGenre.values()) {
+			System.out.print("\nName: " + bookGenre); // toString
+			System.out.print(", name(): " + bookGenre.name());
+			System.out.print(", Ordinal: " + bookGenre.ordinal());
+			System.out.print(", Declaring Class: " + bookGenre.getDeclaringClass());
+			System.out.print(", compareTo(HORROR): " + bookGenre.compareTo(BookGenre.HORROR));
+			System.out.print(", equals(HORROR): " + bookGenre.equals(BookGenre.HORROR));
+		}
+	}
+}
