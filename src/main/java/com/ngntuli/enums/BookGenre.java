90d5b54c676bb3d21221494f8afb3e5881d5d45e
ngntuli <mr.ng.ntuli@gmail.com>
2023-09-09 15:17:08 +0200

    add methods to enums

--- src/main/java/com/ngntuli/enums/BookGenre.java
+++ src/main/java/com/ngntuli/enums/BookGenre.java
@@ -1,16 +1,16 @@
 package com.ngntuli.enums;
 
 public enum BookGenre {
-	BIOGRAPHY, HORROR;
+	BIOGRAPHY(12), HORROR(15);
 
-	public static void main(String[] args) {
-		for (BookGenre bookGenre : BookGenre.values()) {
-			System.out.print("\nName: " + bookGenre); // toString
-			System.out.print(", name(): " + bookGenre.name());
-			System.out.print(", Ordinal: " + bookGenre.ordinal());
-			System.out.print(", Declaring Class: " + bookGenre.getDeclaringClass());
-			System.out.print(", compareTo(HORROR): " + bookGenre.compareTo(BookGenre.HORROR));
-			System.out.print(", equals(HORROR): " + bookGenre.equals(BookGenre.HORROR));
-		}
+	private BookGenre(int minAgeToRead) {
+		this.minAgeToRead = minAgeToRead;
 	}
+
+	private int minAgeToRead;
+
+	public int getMinAgeToRead() {
+		return minAgeToRead;
+	}
+
 }
