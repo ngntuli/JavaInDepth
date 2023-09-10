841c266d1eddcc980321ed582903ce3b0d1f7d9f
ngntuli <mr.ng.ntuli@gmail.com>
2023-09-10 11:58:59 +0200

    add methods

--- src/main/java/com/ngntuli/enums/Book.java
+++ src/main/java/com/ngntuli/enums/Book.java
@@ -2,17 +2,41 @@ package com.ngntuli.enums;
 
 public class Book {
 	public enum BookGenre {
-		BIOGRAPHY(12), HORROR(15);
+		BIOGRAPHY(12) {
+			@Override
+			public boolean isKidFriendly(int age) {
+				return age >= minAgeToRead;
+			}
+		},
+		HORROR(15) {
+			@Override
+			public boolean isKidFriendly(int age) {
+				return false;
+			}
+		};
+
+		public abstract boolean isKidFriendly(int age);
 
 		private BookGenre(int minAgeToRead) {
 			this.minAgeToRead = minAgeToRead;
 		}
 
-		private int minAgeToRead;
+		protected int minAgeToRead;
 
 		public int getMinAgeToRead() {
 			return minAgeToRead;
 		}
+
+//		public boolean isKidFriendly(int age) {
+//			switch (this) {
+//			case BIOGRAPHY:
+//				return age >= minAgeToRead;
+//			case HORROR:
+//				return false;
+//			}
+//			return false;
+//			// throw new AssertionError("Unknown operation: " + this);
+//		}
 
 	}
 }
