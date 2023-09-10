760f193c9a86d70b267d170ac090af482d5a6aac
ngntuli <mr.ng.ntuli@gmail.com>
2023-09-10 11:57:59 +0200

    update method

--- src/main/java/com/ngntuli/enums/EnumsDemo.java
+++ src/main/java/com/ngntuli/enums/EnumsDemo.java
@@ -2,17 +2,6 @@ package com.ngntuli.enums;
 
 public class EnumsDemo {
 	public static void main(String[] args) {
-		for (BookGenre bookGenre : BookGenre.values()) {
-			System.out.println("Name: " + bookGenre); // toString
-			System.out.println("name(): " + bookGenre.name());
-			System.out.println("Ordinal: " + bookGenre.ordinal());
-			System.out.println("Declaring Class: " + bookGenre.getDeclaringClass());
-			System.out.println("compareTo(HORROR): " + bookGenre.compareTo(BookGenre.HORROR));
-			System.out.println("equals(HORROR): " + bookGenre.equals(BookGenre.HORROR));
-			System.out.println("minAgeToRead: " + bookGenre.getMinAgeToRead());
-			System.out.println();
-		}
-		System.out.println("\nBook nested with BookGenre ...");
 		for (com.ngntuli.enums.Book.BookGenre bookGenre : Book.BookGenre.values()) {
 			System.out.println("Name: " + bookGenre); // toString
 			System.out.println("name(): " + bookGenre.name());
@@ -21,6 +10,7 @@ public class EnumsDemo {
 			System.out.println("compareTo(HORROR): " + bookGenre.compareTo(Book.BookGenre.HORROR));
 			System.out.println("equals(HORROR): " + bookGenre.equals(Book.BookGenre.HORROR));
 			System.out.println("minAgeToRead: " + bookGenre.getMinAgeToRead());
+			System.out.println("isKidFriendly: " + bookGenre.isKidFriendly(25));
 			System.out.println();
 		}
 	}
