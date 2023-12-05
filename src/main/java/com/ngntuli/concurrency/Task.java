559cd7e203b2c544c6110270042ee3171265fb6f
ngntuli <ngntuli@eat-sleep-code>
2023-12-05 02:35:13 +0200

    runnable task class

---
+++ src/main/java/com/ngntuli/concurrency/Task.java
@@ -0,0 +1,20 @@
+package com.ngntuli.concurrency;
+
+public class Task implements Runnable {
+
+	@Override
+	public void run() {
+		System.out.println("Inside run ...");
+		go();
+	}
+
+	private void go() {
+		System.out.println("Inside go ...");
+		more();
+	}
+
+	private void more() {
+		System.out.println("Inside more ...");
+	}
+
+}
